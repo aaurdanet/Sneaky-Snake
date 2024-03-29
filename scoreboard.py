37b346dfd4d76907c6036cba5c15b57ea9571534
Antonio Urdaneta <123696971+aaurdanet@users.noreply.github.com>
2024-03-28 22:35:38 -0400

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 
 ALIGNMENT = "center"
-textSize = 24
+textSize = 20
 FONT = ("courier", textSize, "normal")
 
 
@@ -10,19 +10,30 @@ class Scoreboard(Turtle):
         super().__init__()
         self.goto(0, 268)
         self.score_value = 0
+        with open("data.txt", "r") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.update_scoreboard()
         self.shape("blank")
         self.penup()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score_value}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score_value} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score_value > self.high_score:
+            self.high_score = self.score_value
+        self.score_value = 0
+        record = str(self.high_score)
+        with open("data.txt", "w") as data:
+            data.write(record)
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score_value += 1
-        self.clear()
         self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
